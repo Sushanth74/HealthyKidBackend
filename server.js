bad4577aa1710b41d606994c17f2f9cae9e32b6a
Sushanth74 <sushanth4774@gmail.com>
2025-01-28 11:07:10 +0530

    server

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-// server.js
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const authRoutes = require('./routes/auth');
+const authRoutes = require('./routes/auth'); // Import auth routes
 dotenv.config();
+
 const app = express();
 app.use(express.json());
 
@@ -13,9 +13,15 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Health check route
+app.get("/", (req, res) => {
+    res.send("App is running..");
+});
+
 // Routes
-app.use('/routes/auth', authRoutes);
+app.use('/auth', authRoutes); // Correct route path for auth
 
 // Server
 const PORT = process.env.PORT || 5002;
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
